82c7ae08092a49bda49d6ad0a9351b8b209eee48
JuanDavidMarin <testing@vortexbird.com>
2024-02-20 10:27:11 -0500

    UPDATE

---
+++ src/components/main/Hero.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div className='relative flex flex-col w-full h-full'>
+            <img src='/assets/images/icon.png' alt='png' />
+        </div>
+    )
+}
+
+export default Hero
